23298fc7eec83e1034fbb04a876b2f3057ab681e
Atmoslayer <ddsomdim@gmail.com>
2022-12-06 22:19:14 +0300

    Connect service page

--- Beauty_service/views.py
+++ Beauty_service/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', context={})
 
+
+def service(request):
+    return render(request, 'service.html', context={})
