bac4ee7dee66357c1e5580046463ccdf1d72c6b3
Sammy <sammy@shockland.ru>
2022-12-11 19:05:26 +0000

    Update settings

--- BeautyCity/settings.py
+++ BeautyCity/settings.py
@@ -141,3 +141,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://bs.shockland.ru', 'https://127.0.0.1', 'https://localhost']
+
