1da953793d8d8d9a0d6588e08042a04394904537
nikita <bigmama93@mail.ru>
2022-12-11 04:46:06 +0300

    install drf

--- BeautyCity/settings.py
+++ BeautyCity/settings.py
@@ -43,7 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
